1c76ff6169a6a6c53b509aebd3306dde2a61b8bf
Aayushi Prjapati <paayushi63@gmail.com>
2025-02-14 21:17:16 +0530

    Link added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -39,9 +39,9 @@ const {setShowSearch}=useContext(ShopContext);
     </Link>
           <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
             <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
-              <Link>              <p className='cursor-pointer hover:text-black' to='/Profile'>My Profile</p>
+              <Link to='/Profile'>              <p className='cursor-pointer hover:text-black'>My Profile</p>
               </Link>
-              <Link>              <p className='cursor-pointer hover:text-black' to="/orders">Orders</p>
+              <Link to="/orders">              <p className='cursor-pointer hover:text-black' >Orders</p>
               </Link>
                         <p className='cursor-pointer hover:text-black'>Logout</p>
             </div>
