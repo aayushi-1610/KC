72ffbae98ba365c7c736bf4b84f53a990389964b
Aayushi Prjapati <paayushi63@gmail.com>
2025-02-14 21:14:54 +0530

    changes made in navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -39,10 +39,11 @@ const {setShowSearch}=useContext(ShopContext);
     </Link>
           <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
             <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
-              <p className='cursor-pointer hover:text-black'>My Profile</p>
-              <p className='cursor-pointer hover:text-black'>Orders</p>
-              <p className='cursor-pointer hover:text-black'>Logout</p>
-
+              <Link>              <p className='cursor-pointer hover:text-black' to='/Profile'>My Profile</p>
+              </Link>
+              <Link>              <p className='cursor-pointer hover:text-black' to="/orders">Orders</p>
+              </Link>
+                        <p className='cursor-pointer hover:text-black'>Logout</p>
             </div>
           </div>
         </div>
