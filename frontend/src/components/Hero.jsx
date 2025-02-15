ff2fafc4c1a4ae78ce573f00f09f7d2ceca7e3c2
Aayushi Prjapati <paayushi63@gmail.com>
2025-02-15 16:00:38 +0530

    chnages made to images

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { assets } from '../assets/assets'
 const Hero = () => {
   return (
-    <div className='flex flex-col sm:flex-row border border-gray-400'>
+    <div className='h-[65vh] flex flex-col sm:flex-row border border-gray-400 '>
         {/* Hero Left Side*/}
         <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
             <div className='text-[#414141]'>
