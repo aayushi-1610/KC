851275a4aa5d36fb0ddeeff81a3bb6193952c3db
bhavyanshi-k <154896841+bhavyanshi-k@users.noreply.github.com>
2025-02-19 17:23:34 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,18 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import LatestCollection from '../components/LatestCollection'
-import BestSeller from '../components/BestSeller'
-import OurPolicy from '../components/OurPolicy'
-import NewsLetterBox from '../components/NewsLetterBox'
-import About from "./About"
-import SareeCarousel from '../components/slider'
+import React from "react";
+import BestSeller from "../components/BestSeller";
+import OurPolicy from "../components/OurPolicy";
+import About from "./About";
+import SareeCarousel from "../components/slider";
+
 const Home = () => {
   return (
     <div>
       <SareeCarousel></SareeCarousel>
-      <Hero></Hero>
-      <LatestCollection></LatestCollection>
       <BestSeller></BestSeller>
       <About></About>
       <OurPolicy></OurPolicy>
-      <NewsLetterBox></NewsLetterBox>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
